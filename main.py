498f1607521eb7f7af0dc17b887fb14730dcdf5c
Tomás Arjovsky <t.arjovsky@gmail.com>
2017-12-27 13:57:50 -0300

    imports separados, comentarios y directorios.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-from parser import TSP
+from tsp import TSP
+from parser import generar_archivo_nodos_aleatorios
+import time
 
-tsp = TSP("input.txt")
-print(tsp.sol_insercion_mas_cercana())
-# tsp.to_glpk("output_prueba.dat")
+# Cantidad de nodos para cada prueba.
+cantidades = [10, 25, 35, 50, 100, 200]
+
+for cant in cantidades:
+    print ("problema de " + str(cant) + " clientes.")
+    # Archivo aleatorio de input para el TSP por heurística.
+    f_tsp_python = "inputs_tsp_python/" + str(cant) + "clientes.txt"
+    generar_archivo_nodos_aleatorios(f_tsp_python, cant)
+
+    tsp = TSP(f_tsp_python)
+    # Generación de archivo de input para glpk.
+    tsp.to_glpk("inputs_glpk/" + str(cant) + "clientes.dat")
+    # Solución por heurísticas y medición del tiempo.
+    start = time.time()
+    print(tsp.sol_insercion_mas_cercana())
+    end = time.time()
+    print("Tardó " + str(end-start) + " segundos.")
